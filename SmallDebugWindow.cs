60a5e1263088f866b40176261e0b74d5cf95fc79
sea-ex <126366498+sea-ex@users.noreply.github.com>
2023-02-26 20:49:44 +0200

    0.1.3

--- SmallDebugWindow.cs
+++ SmallDebugWindow.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 
 namespace SmallDebugWindow
 {
-    [BepInPlugin("engineering.sea-x.SmallDebugWindow", "Small Debug Window", "0.1.2")]
+    [BepInPlugin("engineering.sea-x.SmallDebugWindow", "Small Debug Window", "0.1.3")]
     public class SmallDebugWindow : BaseUnityPlugin
     {
         public static ManualLogSource ModLogger = null;
